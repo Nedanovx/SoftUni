1236ac19f38671341c90373dcbc0464f1ffe7f16
Nedanovx <nedanovx@gmail.com>
2023-01-29 15:39:07 +0200

    add Generics

---
+++ CSharp-Advanced/Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list = new List<T>();
+        
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public T Remove()
+        {
+            T removedItem = list.Last();
+            list.RemoveAt(list.Count-1);
+            return removedItem;
+        }
+
+        public int Count => list.Count;
+    }
+}
---
+++ CSharp-Advanced/Generics - Lab/03. Generic Scale/EqualityScale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T left;
+        private T right;
+        
+        public EqualityScale(T left, T rigth)
+        {
+            this.left = left;
+            this.right = rigth;
+        }
+
+        public bool AreEqual()
+        {
+            return left.Equals(right);
+        }
+    }
+}
