3ad01765bdd889e3d7c3d9cacbf7e9965d7a2b36
Nedanovx <nedanovx@gmail.com>
2023-09-10 18:30:21 +0300

    commit

---
+++ Data Structures/Trees Representation and Traversal (BFS, DFS)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var subtree = new Tree<int>(36,
+                            new Tree<int>(42),
+                            new Tree<int>(3)
+                            );
+
+            var tree = new Tree<int>(34,
+                        subtree,
+                        new Tree<int>(1,
+                        new Tree<int> (8)),
+                        new Tree<int>(5)
+                );
+
+            Console.WriteLine(string.Join(", ", tree.OrderDfs()));
+            tree.Swap(36, 1);
+            Console.WriteLine(string.Join(", ", tree.OrderDfs()));
+        }
+    }
+}
