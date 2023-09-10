11da232eef9fe1578bb4c956b775090304cc6c59
Nedanovx <nedanovx@gmail.com>
2023-09-10 13:16:08 +0300

    commit

---
+++ MS SQL/Table Relations/Peaks in Rila.sql
@@ -0,0 +1,17 @@
+--9. *Peaks in Rila
+
+USE Geography
+GO
+
+SELECT 
+		m.MountainRange,
+		p.PeakName,
+		p.Elevation
+	FROM Peaks
+	AS p
+JOIN Mountains 
+	AS m
+	ON p.MountainId = m.Id
+	WHERE
+		m.MountainRange = 'Rila'
+	ORDER BY p.Elevation DESC
---
+++ MS SQL/Table Relations/University Database.sql
@@ -0,0 +1,42 @@
+--6. University Database
+
+CREATE DATABASE University
+GO
+
+USE University
+GO
+
+CREATE TABLE Majors
+(
+MajorID INT PRIMARY KEY IDENTITY,
+[Name] VARCHAR (50) NOT NULL
+)
+
+CREATE TABLE Students
+(
+StudentID INT PRIMARY KEY IDENTITY,
+StudentNumber INT NOT NULL,
+StudentName VARCHAR (50) NOT NULL,
+MajorID INT FOREIGN KEY REFERENCES Majors(MajorID) NOT NULL
+)
+
+CREATE TABLE Payments
+(
+PaymentID INT PRIMARY KEY IDENTITY,
+PaymentDate DATETIME2 NOT NULL,
+PaymentAmount DECIMAL (10,2) NOT NULL,
+StudentID INT FOREIGN KEY REFERENCES Students(StudentID) NOT NULL
+)
+
+CREATE TABLE Subjects
+(
+SubjectID INT PRIMARY KEY IDENTITY,
+SubjectName VARCHAR (50) NOT NULL
+)
+
+CREATE TABLE Agenda
+(
+StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID),
+PRIMARY KEY (StudentID, SubjectID)
+)
