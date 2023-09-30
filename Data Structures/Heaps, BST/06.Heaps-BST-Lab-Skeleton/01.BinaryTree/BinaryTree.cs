52e10be3078858fcd27d4649912207d37963e337
Nedanovx <nedanovx@gmail.com>
2023-09-30 21:42:52 +0300

    commit

---
+++ Data Structures/Heaps, BST/06.Heaps-BST-Lab-Skeleton/01.BinaryTree/BinaryTree.cs
@@ -0,0 +1,115 @@
+﻿namespace _01.BinaryTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class BinaryTree<T> : IAbstractBinaryTree<T>
+    {
+        public BinaryTree(T element, IAbstractBinaryTree<T> left, IAbstractBinaryTree<T> right)
+        {
+            this.Value = element;
+            this.LeftChild = left;
+            this.RightChild = right;
+        }
+
+        public T Value { get; private set; }
+
+        public IAbstractBinaryTree<T> LeftChild { get; private set; }
+
+        public IAbstractBinaryTree<T> RightChild { get; private set; }
+
+        public string AsIndentedPreOrder(int indent)
+        {
+            var sb = new StringBuilder();
+            this.PreOrderDfs(sb, indent, this);
+            return sb.ToString();
+        }
+
+        private void PreOrderDfs(StringBuilder sb, int indent, IAbstractBinaryTree<T> binaryTree)
+        {
+            sb.Append(new String(' ', indent)).AppendLine(binaryTree.Value.ToString());
+
+            if(binaryTree.LeftChild != null)
+            {
+                this.PreOrderDfs(sb, indent + 2, binaryTree.LeftChild);
+            }
+
+            if(binaryTree.RightChild != null)
+            {
+                this.PreOrderDfs(sb, indent + 2, binaryTree.RightChild);
+            }
+        }
+
+        public void ForEachInOrder(Action<T> action)
+        {
+            if(this.LeftChild != null)
+            {
+                this.LeftChild.ForEachInOrder(action);
+            }
+            action.Invoke(Value);
+
+            if(this.RightChild != null)
+            {
+                this.RightChild.ForEachInOrder(action);
+            }
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> InOrder()
+        {
+            var result = new List<IAbstractBinaryTree<T>>();
+
+            if (this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.InOrder());
+            }
+
+            result.Add(this);
+
+            if (this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.InOrder());
+            }         
+
+            return result;
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> PostOrder()
+        {
+            var result = new List<IAbstractBinaryTree<T>>();
+
+            if (this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.PostOrder());
+            }
+
+            if (this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.PostOrder());
+            }
+
+            result.Add(this);
+
+            return result;
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> PreOrder()
+        {
+            var result = new List<IAbstractBinaryTree<T>>();
+
+            result.Add(this);
+
+            if(this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.PreOrder());
+            }
+
+            if(this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.PreOrder());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Data Structures/Heaps, BST/06.Heaps-BST-Lab-Skeleton/03.MaxHeap/MaxHeap.cs
@@ -0,0 +1,103 @@
+﻿namespace _03.MaxHeap
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MaxHeap<T> : IAbstractHeap<T> where T : IComparable<T>
+    {
+        private List<T> elements;
+
+        public MaxHeap()
+        {
+            this.elements = new List<T>();
+        }
+        public int Size => this.elements.Count;
+
+        public void Add(T element)
+        {
+            this.elements.Add(element);
+            this.HeapifyUp(this.elements.Count - 1);
+        }
+
+        private void HeapifyUp(int index)
+        {
+            var parentIndex = (index - 1) / 2;
+            while(index > 0 && this.elements[index].CompareTo(this.elements[parentIndex]) > 0)
+            {
+                this.Swap(index, parentIndex);
+                index = parentIndex;
+                parentIndex = (index - 1) / 2;
+            }
+        }
+
+        private void Swap(int index, int parentIndex)
+        {
+            var temp = this.elements[index];
+            this.elements[index] = this.elements[parentIndex];
+            this.elements[parentIndex] = temp;
+        }
+
+        public T ExtractMax()
+        {
+            if (this.elements.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            T element = this.elements[0];
+            this.Swap(0, this.elements.Count - 1);
+            this.elements.RemoveAt(this.elements.Count - 1);
+            this.HeapifyDown(0);
+            return element;
+        }
+
+        private void HeapifyDown(int index)
+        {
+            var biggerChildIndex = this.GetBiggerChildIndex(index);
+            while(biggerChildIndex >= 0 && biggerChildIndex < this.elements.Count && this.elements[biggerChildIndex].CompareTo(this.elements[index]) > 0)
+            {
+                this.Swap(biggerChildIndex, index);
+                index = biggerChildIndex;
+                biggerChildIndex = this.GetBiggerChildIndex(index);
+            }
+        }
+
+        private int GetBiggerChildIndex(int index)
+        {
+            var firstChildIndex = index * 2 + 1;
+            var secondChildIndex = index * 2 + 2;
+            if(secondChildIndex < this.elements.Count)
+            {
+                if (this.elements[firstChildIndex].CompareTo(this.elements[secondChildIndex]) > 0)
+                {
+                    return firstChildIndex;
+                }
+
+                return secondChildIndex;
+            }
+            else if(firstChildIndex < this.elements.Count)
+            {
+                return firstChildIndex;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        private void RemoveAt(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            if(this.elements.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.elements[0];
+        }
+    }
+}
