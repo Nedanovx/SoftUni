be1ac78b641a4ad20201ac6fc68ffeba66724f48
Nedanovx <nedanovx@gmail.com>
2023-10-04 00:00:25 +0300

    commit

---
+++ Data Structures/Exam preparation/Regular Exam - 11 December 2022/Exam.AirlinesManager/AirlinesManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam.DeliveriesManager
+{
+    public class AirlinesManager : IAirlinesManager
+    {
+
+        private Dictionary<string, Airline> airlinesById = new Dictionary<string, Airline>();
+        private Dictionary<string, Flight> flightById = new Dictionary<string, Flight>();
+        public void AddAirline(Airline airline)
+        {
+            airlinesById.Add(airline.Id, airline);
+        }
+
+        public void AddFlight(Airline airline, Flight flight)
+        {
+            if (!airlinesById.ContainsKey(airline.Id))
+            {
+                throw new ArgumentException();
+            }
+            airlinesById[airline.Id].Flights.Add(flight);
+            flightById.Add(flight.Id, flight);
+        }
+
+        public bool Contains(Airline airline)
+        {
+            return airlinesById.ContainsKey(airline.Id);
+        }
+
+        public bool Contains(Flight flight)
+        {
+            return flightById.ContainsKey(flight.Id);
+        }
+
+        public void DeleteAirline(Airline airline)
+        {
+            if (!airlinesById.ContainsKey(airline.Id))
+            {
+                throw new ArgumentException();
+            }
+
+            foreach(var flight in flightById.Values)
+            {
+              flightById.Remove(flight.Id);
+            }
+
+            airlinesById.Remove(airline.Id);
+
+
+        }
+
+        public IEnumerable<Airline> GetAirlinesOrderedByRatingThenByCountOfFlightsThenByName()
+        {
+            return airlinesById.Select(a => a.Value).OrderByDescending(x => x.Rating).ThenByDescending(a => a.Flights.Count).ThenBy(a => a.Name);
+        }
+
+        public IEnumerable<Airline> GetAirlinesWithFlightsFromOriginToDestination(string origin, string destination)
+        {
+            return airlinesById.Select(a => a.Value).Where(x => x.Flights.Any(f => f.Destination == destination && f.Origin == origin && f.IsCompleted == false));
+        }
+
+        public IEnumerable<Flight> GetAllFlights()
+        {
+            return flightById.Values;
+        }
+
+        public IEnumerable<Flight> GetCompletedFlights()
+        {
+            return flightById.Values.Where(x => x.IsCompleted);
+        }
+
+        public IEnumerable<Flight> GetFlightsOrderedByCompletionThenByNumber()
+        {
+            return flightById.Values.OrderBy(x => x.IsCompleted).ThenBy(x => x.Number);
+        }
+
+        public Flight PerformFlight(Airline airline, Flight flight)
+        {
+            if (!flightById.ContainsKey(flight.Id) || !airlinesById.ContainsKey(airline.Id))
+            {
+                throw new ArgumentException();
+            }
+            flightById[flight.Id].IsCompleted = true;
+            return flightById[flight.Id];
+        }
+    }
+}
---
+++ Data Structures/Exam preparation/Regular Exam - 11 December 2022/Exam.DeliveriesManager/DeliveriesManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exam.DeliveriesManager
+{
+    public class DeliveriesManager : IDeliveriesManager
+    {
+
+        private Dictionary<string, Deliverer> deliverersById = new Dictionary<string, Deliverer>();
+        private Dictionary<string, Package> packageById = new Dictionary<string, Package>();
+        private Dictionary<string, string> packagesByDeliverer = new Dictionary<string, string>();
+
+        public void AddDeliverer(Deliverer deliverer)
+        {
+            deliverersById.Add(deliverer.Id, deliverer);
+        }
+
+        public void AddPackage(Package package)
+        {
+            packageById.Add(package.Id, package);
+        }
+
+        public void AssignPackage(Deliverer deliverer, Package package)
+        {
+            if (!deliverersById.ContainsKey(deliverer.Id) || !packageById.ContainsKey(package.Id))
+            {
+                throw new ArgumentException();
+            }
+            packagesByDeliverer.Add(package.Id, deliverer.Id);
+        }
+
+        public bool Contains(Deliverer deliverer)
+        {
+            return deliverersById.ContainsKey(deliverer.Id);
+        }
+
+        public bool Contains(Package package)
+        {
+            return packageById.ContainsKey(package.Id);
+        }
+
+        public IEnumerable<Deliverer> GetDeliverers()
+        {
+            return deliverersById.Values;
+        }
+
+        public IEnumerable<Deliverer> GetDeliverersOrderedByCountOfPackagesThenByName()
+        {
+            var deliverersByPackageId = new Dictionary<string, int>();
+
+            foreach(var kvp in packagesByDeliverer)
+            {
+                if(!deliverersByPackageId.ContainsKey(kvp.Value))
+                {
+                    deliverersByPackageId.Add(kvp.Value,0);
+                }
+                deliverersByPackageId[kvp.Value] += 1;
+            }
+            return deliverersByPackageId.OrderByDescending(kvp => kvp.Value)
+                .ThenBy(kvp => deliverersById[kvp.Key].Name)
+                .Select(kvp => deliverersById[kvp.Key]); 
+        }
+
+        public IEnumerable<Package> GetPackages()
+        {
+            return packageById.Values;
+        }
+
+        public IEnumerable<Package> GetPackagesOrderedByWeightThenByReceiver()
+        {
+            return packageById.Values.OrderByDescending(package => package.Weight).ThenBy(package => package.Receiver);
+        }
+
+        public IEnumerable<Package> GetUnassignedPackages()
+        {
+            return packageById.Where(package => !packagesByDeliverer.ContainsKey(package.Key)).Select(package => package.Value);
+        }
+    }
+}
