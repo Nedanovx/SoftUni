e7633f4ad46abbc480d97fa0dab9f60f9b795793
Nedanovx <nedanovx@gmail.com>
2024-01-27 12:28:32 +0200

    commit

---
+++ ASP.NET Fundamentals/Exercise ASP.NET Core Introduction/TextSplitter/TextSplitter/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSplitter.Models;
+
+namespace TextSplitter.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index(TextSplitViewModel textViewModel)
+		{
+			return View(textViewModel);
+		}
+
+		[HttpPost]
+		public IActionResult Split(TextSplitViewModel textViewModel)
+		{
+			if(!ModelState.IsValid)
+			{
+				return RedirectToAction("Index", textViewModel);
+			}
+
+			string[] words = textViewModel.Text.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+			string result = string.Join(Environment.NewLine, words);
+			textViewModel.SplitText = result;
+			return RedirectToAction("Index", textViewModel);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ ASP.NET Fundamentals/Exercise ASP.NET Core Introduction/TextSplitter/TextSplitter/Models/TextSplitViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitter.Models
+{
+	public class TextSplitViewModel
+	{
+		[Required]
+		[StringLength(30, MinimumLength = 2, ErrorMessage = "The field Text must be a string with a minimum length of 2 and \r\nmaximum length of 30.")]
+		public string Text { get; set; } = null!;
+
+		public string? SplitText { get; set; }
+	}
+}
